c1909d94da63e41116532743a3535df4010070f1
Charles Chege <charleschege@protonmail.ch>
2021-09-28 13:06:33 +0200

    Add constants for global values

--- src/constants.rs
+++ src/constants.rs
@@ -1,21 +1,99 @@
-/// The number of nanoseconds in a microsecond.
-pub const NANOS_PER_MICRO: u32 = 1000;
-/// The number of nanoseconds in a millisecond.
-pub const NANOS_PER_MILLI: u32 = 1000_000;
-/// The number of nanoseconds in seconds.
-pub const NANOS_PER_SEC: u32 = 1_000_000_000;
-/// The number of microseconds per second.
-pub const MICROS_PER_SEC: i64 = 1000_000;
-/// The number of milliseconds per second.
-pub const MILLIS_PER_SEC: i64 = 1000;
-/// The number of seconds in one minute.
-pub const SECONDS_PER_MINUTE: u64 = 60;
+use core::fmt;
 
-/// The number of seconds in one hour.
-pub const SECONDS_PER_HOUR: u64 = 60 * SECONDS_PER_MINUTE;
+pub(crate) const NOT_LEAP_YEAR_MONTHS: [i64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+pub(crate) const LEAP_YEAR_MONTHS: [i64; 12] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-/// The number of seconds in one day.
-pub const SECONDS_PER_DAY: u64 = 24 * SECONDS_PER_HOUR;
+pub type UnixTimestamp = i64;
 
-/// The number of seconds in one week.
-pub const SECONDS_PER_WEEK: u64 = 7 * SECONDS_PER_DAY;
+pub enum YearType {
+    IsLeapYear,
+    NotLeapYear,
+}
+
+impl fmt::Debug for YearType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IsLeapYear => write!(f, "{}", "IsLeapYear"),
+            Self::NotLeapYear => write!(f, "{}", "NotLeapYear"),
+        }
+    }
+}
+
+impl fmt::Display for YearType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IsLeapYear => write!(f, "{}", "YearType::IsLeapYear"),
+            Self::NotLeapYear => write!(f, "{}", "YearType::NotLeapYear"),
+        }
+    }
+}
+
+impl core::cmp::PartialEq for YearType {
+    fn eq(&self, other: &Self) -> bool {
+        if self == other {
+            true
+        } else {
+            false
+        }
+    }
+}
+impl core::cmp::Eq for YearType {}
+
+impl core::marker::Copy for YearType {}
+
+impl core::clone::Clone for YearType {
+    fn clone(&self) -> YearType {
+        *self
+    }
+}
+
+pub enum TimeStandard {
+    Utc,
+    Tai64,
+    Tai64N,
+    Tai64NA,
+    UtcOffset,
+}
+
+impl fmt::Debug for TimeStandard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Utc => write!(f, "{}", "Utc"),
+            Self::Tai64 => write!(f, "{}", "Tai64"),
+            Self::Tai64N => write!(f, "{}", "Tai64N"),
+            Self::Tai64NA => write!(f, "{}", "Tai64NA"),
+            Self::UtcOffset => write!(f, "{}", "UtcOffset"),
+        }
+    }
+}
+
+impl fmt::Display for TimeStandard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Utc => write!(f, "{}", "TimeStandard::UTC"),
+            Self::Tai64 => write!(f, "{}", "TimeStandard::TAI64"),
+            Self::Tai64N => write!(f, "{}", "TimeStandard::TAI64N"),
+            Self::Tai64NA => write!(f, "{}", "TimeStandard::TAI64NA"),
+            Self::UtcOffset => write!(f, "{}", "TimeStandard::UTC OFFSET"),
+        }
+    }
+}
+
+impl core::cmp::PartialEq for TimeStandard {
+    fn eq(&self, other: &Self) -> bool {
+        if self == other {
+            true
+        } else {
+            false
+        }
+    }
+}
+impl core::cmp::Eq for TimeStandard {}
+
+impl core::marker::Copy for TimeStandard {}
+
+impl core::clone::Clone for TimeStandard {
+    fn clone(&self) -> TimeStandard {
+        *self
+    }
+}
